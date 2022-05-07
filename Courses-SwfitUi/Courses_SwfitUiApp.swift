843a6601b594ead636c7eef999001354b8286c65
mmaignardi <matheus.maignardi@meliuz.com.br>
2022-05-07 15:09:48 -0400

    Initial Commit

---
+++ Courses-SwfitUi/Courses_SwfitUiApp.swift
@@ -0,0 +1,17 @@
+//
+//  Courses_SwfitUiApp.swift
+//  Courses-SwfitUi
+//
+//  Created by Matheus Mendonça Maignardi on 07/05/22.
+//
+
+import SwiftUI
+
+@main
+struct Courses_SwfitUiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
